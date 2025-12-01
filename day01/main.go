6a505d02a316f5184a09550746dc55f87ab7e957
Christian Kirkegaard <christian@lowpoly.dk>
2025-12-01 15:34:50 +0100

    Add day 1 part 2

--- day01/main.go
+++ day01/main.go
@@ -6,35 +6,51 @@ import (
 	"strconv"
 )
 
-func wrap(value int, min int, max int) int {
-	rangeSize := max - min + 1
-	v := (value - min) % rangeSize
-	if v < 0 {
-		v += rangeSize
+func wrap(position int, signedSteps int) (int, int) {
+	newPosition := (position + signedSteps) % 100
+	if newPosition < 0 {
+		newPosition += 100
 	}
-	return v + min
+
+	zeroCrossings := 0
+	newPos := position + signedSteps
+
+	if newPos > 0 {
+		zeroCrossings = newPos / 100
+	} else if newPos == 0 {
+		zeroCrossings = 1
+	} else {
+		zeroCrossings = ((100-position)%100 + (-signedSteps)) / 100
+	}
+
+	return newPosition, zeroCrossings
 }
 
 func main() {
-	result := 0
-	dial := 50
 	input := utils.ReadFile("./day01/input")
+
+	result1 := 0
+	result2 := 0
+	dial := 50
 
 	for _, line := range input {
 		direction := string(line[0])
 		steps, _ := strconv.Atoi(line[1:])
 
-		switch direction {
-		case "R":
-			dial = wrap(dial+steps, 0, 99)
-		case "L":
-			dial = wrap(dial-steps, 0, 99)
+		signedSteps := steps
+		if direction == "L" {
+			signedSteps = -steps
 		}
+
+		zeroes := 0
+		dial, zeroes = wrap(dial, signedSteps)
 
 		if dial == 0 {
-			result++
+			result1++
 		}
+		result2 += zeroes
 	}
 
-	fmt.Println(result)
+	fmt.Println("Part 1:", result1)
+	fmt.Println("Part 2:", result2)
 }
